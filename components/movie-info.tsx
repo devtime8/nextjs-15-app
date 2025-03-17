aadc83a5c6bd61a3308c0a06241c2b15b99b8af0
devtime8 <devtime8@gmail.com>
2025-03-18 04:31:47 +0900

    format

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,51 +1,51 @@
-import { notFound } from "next/navigation"
-import { API_URL } from "../app/(home)/page"
-import { fetchMovieDetails } from "../app/actions"
+import { notFound } from "next/navigation";
+import { API_URL } from "../app/(home)/page";
+import { fetchMovieDetails } from "../app/actions";
 
+import styles from "../styles/movie-info.module.css";
 
+// interface Post {
+//     id: string
+//     title: string
+//     content: string
+//     error?: string
+//   }
 
+// async function getPost(id: string) {
 
-interface Post {
-    id: string
-    title: string
-    content: string
-    error?: string
-  }
+//     // await new Promise ((res)=> setTimeout(res, 5000))
+//     const res = await fetch(`${API_URL}/${id}`, {
+//     //   cache: 'force-cache',
+//     })
+//     const post: Post = await res.json()
 
-async function getPost(id: string) {
+//     console.log(`■■■ id=${id}`)
+//     console.log(`■■■ post=${post}`)
+//     console.log(`■■■ post=${JSON.stringify(post)}`)
+//     if (!post  ) notFound()
+//     if (post.error) {
+//         console.log(post.error)
+//     }
 
-    // await new Promise ((res)=> setTimeout(res, 5000))
-    const res = await fetch(`${API_URL}/${id}`, {
-    //   cache: 'force-cache',
-    })
-    const post: Post = await res.json()
+//     return post
+//   }
 
-    console.log(`■■■ id=${id}`)
-    console.log(`■■■ post=${post}`)
-    console.log(`■■■ post=${JSON.stringify(post)}`)
-    if (!post  ) notFound()
-    if (post.error) {
-        console.log(post.error) 
-    }
+export default async function MovieInfo({ id }: { id: string }) {
+  // const post = await getPost(id)
+  const movie = await fetchMovieDetails(id);
+  console.log(movie);
 
-    return post
-  }
-
-
-
-export default async function MovieInfo({id}: {id:string}){
-
-    const post = await getPost(id)
-    const movie = await fetchMovieDetails(id);
-    console.log(movie)
-
-    return <div> 
-            <h1>{movie.original_title}</h1>
-            <img  
-    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} 
-    alt={movie.title}  />
-    <img  
-src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-alt={movie.title}  />
+  return (
+    <div className={styles.container}>
+      <h1>{movie.original_title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+        alt={movie.title}
+      />
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+      />
     </div>
+  );
 }
